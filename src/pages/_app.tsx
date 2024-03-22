84935ee5c3f6a1468dc3cc1b46112fa84145fbe0
Leandro Corso <contato@leandrocorso.com.br>
2024-03-22 17:23:41 -0300

    styled-components setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import { ThemeProvider, type DefaultTheme } from "styled-components";
+import { GlobalStyle, defaultTheme } from "@/styles";
 import type { AppProps } from "next/app";
 
+const theme: DefaultTheme = defaultTheme;
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
