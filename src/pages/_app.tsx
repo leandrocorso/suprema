70c117e12a0043a7af71eaba4939c5aae515d2d7
Leandro Corso <contato@leandrocorso.com.br>
2024-03-25 09:56:39 -0300

    React-query setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import { ThemeProvider, type DefaultTheme } from "styled-components";
 import { GlobalStyle, defaultTheme } from "@/styles";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const theme: DefaultTheme = defaultTheme;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
