655462c92ba6aad0b782dcae95e22d85ce46c066
Leandro Corso <contato@leandrocorso.com.br>
2024-03-25 23:41:17 -0300

    initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
+import Head from "next/head";
+import { useQuery } from "@tanstack/react-query";
+
+import { Navbar } from "@/components/Navbar";
+import { getPokemons } from "@/services/pokeapi";
+import { PokemonList } from "@/components/PokemonList";
+
 export default function Home() {
+  const {
+    data: pokemonList,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["pokemonList"],
+    queryFn: getPokemons,
+  });
+
   return (
-    <main>
-      <h1>Suprema Gaming & Co</h1>
-    </main>
+    <>
+      <Head>
+        <title>Pokémons</title>
+      </Head>
+      <main>
+        <Navbar />
+        <div className="container">
+          <h1>Suprema Gaming & Co</h1>
+
+          <div className="pokemons">
+            {isError && <p>Ocorreu um erro</p>}
+            {isLoading && <p>Carregando</p>}
+            {pokemonList && <PokemonList pokemons={pokemonList} />}
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
