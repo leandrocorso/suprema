7b67658b61d80a6b5ae09b09574e76f18039272f
Leandro Corso <contato@leandrocorso.com.br>
2024-03-26 22:32:28 -0300

    applying Layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,45 @@
-import Head from "next/head";
-import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 
-import { Navbar } from "@/components/Navbar";
-import { getPokemons } from "@/services/pokeapi";
+import { useQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+
+import { Layout } from "@/components/Layout";
 import { PokemonList } from "@/components/PokemonList";
 
+import { getPokemons } from "@/services/pokeapi";
+import { PokemonProps } from "@/types/Pokemon";
+
 export default function Home() {
-  const {
-    data: pokemonList,
-    isError,
-    isLoading,
-  } = useQuery({
-    queryKey: ["pokemonList"],
-    queryFn: getPokemons,
+  const limit = 4; // pokemons per page
+  const [offset, setOffset] = useState<number>(0);
+  const [pokemonList, setPokemonList] = useState<PokemonProps[]>([]);
+
+  const { ref, inView } = useInView();
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["pokemonList", offset],
+    queryFn: async () => await getPokemons(offset),
   });
 
-  return (
-    <>
-      <Head>
-        <title>Pokémons</title>
-      </Head>
-      <main>
-        <Navbar />
-        <div className="container">
-          <h1>Suprema Gaming & Co</h1>
+  useEffect(() => {
+    data && setPokemonList((prev) => [...prev, ...data]);
+  }, [data]);
 
-          <div className="pokemons">
-            {isError && <p>Ocorreu um erro</p>}
-            {pokemonList && (
-              <PokemonList pokemons={pokemonList} isLoading={isLoading} />
-            )}
-          </div>
-        </div>
-      </main>
-    </>
+  useEffect(() => {
+    inView && setOffset(offset + limit);
+  }, [inView]);
+
+  return (
+    <Layout title="Home">
+      <h1>Home</h1>
+
+      {isError && <p>Ocorreu um erro</p>}
+      {pokemonList.length && (
+        <>
+          <PokemonList pokemons={pokemonList || []} isLoading={isLoading} />
+          <span ref={ref} />
+        </>
+      )}
+    </Layout>
   );
 }
